d6fb024b21dce00cf7537f1a9ae473082582695a
rahimkpm <pmvkatp@gmail.com>
2026-01-28 22:19:50 +0530

    gitpages

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { provideHttpClient } from '@angular/common/http';
 import { EmployeeComponent } from './app/employee/employee';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import test from 'node:test';
 import { Test } from './app/test/test';
+import { routes } from './app/app.routes';
 
 // bootstrapApplication(App, appConfig)
 //   .catch((err) => console.error(err));
 
   bootstrapApplication(App,appConfig);
-  providers: [provideHttpClient() ]     // <-- registers HttpClient
+  providers: [provideHttpClient(),provideRouter(routes, withHashLocation()) ]     // <-- registers HttpClient
   
